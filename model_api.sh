ae0431149b55ce10b0d407dc4418013d597ffbe4
mhy-u <mohy_cpu@163.com>
2025-07-08 20:27:22 +0800

    add api test

--- model_api.sh
+++ model_api.sh
@@ -4,5 +4,10 @@ python src/api_demo.py \
 
 # python src/api_demo.py \
 #     --model_name_or_path model/chinese-alpaca-2-7b \
-#     --template llama2_zh
+#     --template llama2_zh \
+#     > log.txt 2>&1
+
+# python src/api_demo.py \
+#     --model_name_or_path model/chatglm3-6b \
+#     --template chatglm3 \
 #     > log.txt 2>&1
